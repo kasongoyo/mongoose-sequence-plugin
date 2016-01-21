2d029d88f95a92d2e37dc74abd95004ffcf34460
kasongoyo <kasongoyo@gmail.co>
2016-01-21 11:54:11 +0300

    fix bug concern mongoose conflict btn plugin and application

--- test/models/sample_model.js
+++ test/models/sample_model.js
@@ -2,6 +2,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var GeneratorModel = mongoose.model('IdGenerator');
+
 var idAutoGenerator = require('../../index');
 
 //sample schema
@@ -19,7 +21,8 @@ var SampleSchema = new Schema({
 SampleSchema.plugin(idAutoGenerator, {
     field: 'code',
     idName: 'SAMPLE_CODE',
-    startAt: '99-ZX'
+    startAt: '99-ZX',
+    generatorModel: GeneratorModel
 });
 
 module.exports = mongoose.model('Sample', SampleSchema);
